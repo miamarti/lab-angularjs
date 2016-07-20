fffbc7fc77ff1ed94d82f03ec330ae5f95570ba4
Miller Augusto <miller@ciandt.com>
2016-07-20 16:48:15 -0300

    Start

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name labApp
+ * @description
+ * # labApp
+ *
+ * Main module of the application.
+ */
+angular.module('labApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ng.ckeditor'
+]).config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html',
+            controller: 'AboutCtrl',
+            controllerAs: 'about'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name labApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the labApp
+ */
+angular.module('labApp').controller('MainCtrl', function () {
+
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    this.htmlEditor = 'XXXXXXXXXXXX';
+
+});
